5ced9037f14bd45e231999c1174e5bb177d6b5ef
karam-khanna <84152938+karam-khanna@users.noreply.github.com>
2023-02-15 08:29:32 -0500

    major upgrades + tracking data now

--- main.py
+++ main.py
@@ -2,58 +2,153 @@
 
 # Press ⇧F10 to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+import os
 
 from vpython import *
 from numpy import random
 
 import pyvisgraph as vg
 
+import pandas as pd
+
+
 
 
 # create class called capacity_simulation
 class sim:
+
 
     class room:
         basic = 0
         classroom = 1
         meetingRoom = 2
         jonesRoom = 3
+        rooms = [basic, classroom, meetingRoom, jonesRoom]
+        roomNumberToName = {0: "basic", 1: "classroom", 2: "meetingRoom", 3: "jonesRoom"}
+        roomCapacity = {basic: 100, classroom: 100, meetingRoom: 25, jonesRoom: 200}
 
+    doorWidth = 3
 
+    # ------------------#
 
-    numHumans = 10
+    numHumans = 200
     humanRadius = .5
 
     # reccd at 5
-    baseVelocity = 5
+    baseVelocity = 4.7
 
     # this sets the minimum speed percentage slowdown based on having someone in front
     minSpeed = 0.001
 
-
     # this sets the slowest natural speed percentage slowdown for a person
-    minInitialSpeedCoefficient = 0.8
+    minInitialSpeedCoefficient = 0.6
 
-    # reccd at 120
-    coneWidth = 60
+    # do not set above 90
+    coneWidth = 90
 
     # reccd at 1
     coneRadius = 1
 
-
     showLabels = False
-    maxWaitCount = 100
+
+    # speed of door is width of door / speed of people
+    secondsTakenToExitPP = (doorWidth / (baseVelocity * .5)) / (doorWidth / (humanRadius * 2))
 
     ## reccd at .4
     likelihoodOfWaiting = 0.4
 
+    roomName = room.jonesRoom
 
-    roomName = room.classroom
+    # ------------------#
 
+    if roomName == room.basic:
+        dt = 0.01
+    elif roomName == room.classroom:
+        dt = 0.01
+    elif roomName == room.meetingRoom:
+        dt = 0.01
+    elif roomName == room.jonesRoom:
+        if numHumans > 160:
+            dt = 0.05
+        if numHumans > 120:
+            dt = 0.04
+        else:
+            dt = 0.02
 
-    # class vars
+    # Don't touch ME ------------------#
     rectangles = None
     graph = None
+    framesTakenToExit = secondsTakenToExitPP / dt
+    doorIsBeingUsed = False
+    doorWidth = 3
+    toDelete = []
+
+
+
+
+        # add max capacity for each room
+
+
+    def reset_variables(self):
+
+        doorWidth = 3
+        maxCapacity = {"basic": 100, "classroom": 100, "meetingRoom": 25, "jonesRoom": 200}
+
+        # ------------------#
+
+        numHumans = 200
+        humanRadius = .5
+
+        # reccd at 5
+        baseVelocity = 4.7
+
+        # this sets the minimum speed percentage slowdown based on having someone in front
+        minSpeed = 0.001
+
+        # this sets the slowest natural speed percentage slowdown for a person
+        minInitialSpeedCoefficient = 0.6
+
+        # do not set above 90
+        coneWidth = 90
+
+        # reccd at 1
+        coneRadius = 1
+
+        showLabels = False
+
+        # speed of door is width of door / speed of people
+        secondsTakenToExitPP = (doorWidth / (baseVelocity * .5)) / (doorWidth / (humanRadius * 2))
+
+        ## reccd at .4
+        likelihoodOfWaiting = 0.4
+
+        roomName = sim.room.jonesRoom
+
+        # ------------------#
+
+        if roomName == sim.room.basic:
+            dt = 0.01
+        elif roomName == sim.room.classroom:
+            dt = 0.01
+        elif roomName == sim.room.meetingRoom:
+            dt = 0.01
+        elif roomName == sim.room.jonesRoom:
+            if numHumans > 160:
+                dt = 0.05
+            if numHumans > 120:
+                dt = 0.04
+            else:
+                dt = 0.02
+
+        # Don't touch ME ------------------#
+        rectangles = None
+        graph = None
+        framesTakenToExit = secondsTakenToExitPP / dt
+        doorIsBeingUsed = False
+        doorWidth = 3
+        # ------------------#
+
+
 
 
 
@@ -93,22 +188,33 @@ class sim:
 
         # special case where interim goal is the last one
         if len(person.interimGoals) == 1:
-            successRadius = successRadius*4
+            successRadius = successRadius*10
+
 
 
         # check to see if at current point
         if mag(curPos - nextPoint) < successRadius:
 
             # remove current goal from interimGoals
-            person.interimGoals.pop(0)
+
 
             # check to see if there are any more goals
-            if len(person.interimGoals) == 0:
-                person.visible = False
+            if len(person.interimGoals) == 1:
 
-                # remove from people
-                people.remove(person)
+                if not sim.doorIsBeingUsed or person.isUsingDoor:
+                    sim.doorIsBeingUsed = True
+                    person.waitCount += 1
+                    person.isUsingDoor = True
+
+                    if person.waitCount > sim.framesTakenToExit:
+                        sim.doorIsBeingUsed = False
+                        person.isUsingDoor = False
+                        person.waitCount = 0
+                        person.visible = False
+                        people.remove(person)
+
                 return 0, 0
+            person.interimGoals.pop(0)
 
         # get next goal in interimGoals
         nextPoint = person.interimGoals[0]
@@ -186,7 +292,7 @@ class sim:
             # scaledDistance = 1 / (1 + exp(-closestPersonDistance*10 + 5))
             # print(closestPersonDistance)
             if closestPersonDistance > 0:
-                velocity = sqrt(closestPersonDistance)
+                velocity = max(sim.minSpeed, sqrt(closestPersonDistance))
 
             if closestPersonDistance <= 0:
                 velocity = sim.minSpeed
@@ -295,10 +401,15 @@ class sim:
             rectangles.append(obstacle4_barrier)
             rectangles.append(obstacle5_barrier)
 
+
+
+
             goalLocation = vector(7, 7, sim.humanRadius)
             goal = box(pos=goalLocation, size=vector(1.5, 1.5, 2), color=color.green)
 
-            return
+            sim.toDelete = [obstacle1_barrier, obstacle2_barrier, obstacle3_barier, obstacle4_barrier,
+                        obstacle5_barrier, obstacle1_visible, obstacle2_visible, obstacle3_visible,
+                        obstacle4_visible, obstacle5_visible, wallR, wallL, wallB, wallT, goalLocation, goal, floor]
 
 
 
@@ -333,6 +444,9 @@ class sim:
             # add image that looks like door on door
             doorImage = box(pos=vector(-5.5, 3.5, 1), size=vector(3, 0.5, 3), color=color.white, opacity=1)
             doorImage.texture = textures.wood_old
+
+            # write toDelete with all created objects
+            sim.toDelete = [wallR, wallL, wallB, wallT, table, table_barrier, goalLocation, goal, doorImage, floor]
 
 
         if sim.roomName == sim.room.classroom:
@@ -390,6 +504,8 @@ class sim:
 
             rectangles = [desk1_barrier, desk2_barrier, desk3_barrier, desk4_barrier, teachers_barrier]
 
+            # write toDelete with all created objects
+            sim.toDelete = [wallR, wallL, wallB, wallT, floor, goalLocation, goal, door, desk1, desk2, desk3, desk4, teachers, rectangles, desk1_barrier, desk2_barrier, desk3_barrier, desk4_barrier, teachers_barrier]
 
         if sim.roomName == sim.room.jonesRoom:
             floor = box(pos=vector(0, 0, -1), size=vector(26, 28, 2), texture=textures.rug)
@@ -408,25 +524,8 @@ class sim:
 
             rectangles = []
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+            # write toDelete with all created objects
+            sim.toDelete = [wallR, wallL, wallB, wallT, floor, goalLocation, goal]
 
 
         # -------------------------- create vg polygons --------------------------#
@@ -463,15 +562,17 @@ class sim:
 
                 # person = sphere(pos=vector(random.uniform(-7, 7), random.uniform(-7, 7), sim.humanRadius), radius=sim.humanRadius, color=color.red)
                 person = sphere(pos=vector(random.uniform(roomCoordinates["xmin"] + 2*sim.humanRadius, roomCoordinates["xmax"] - 2*sim.humanRadius), random.uniform(roomCoordinates["ymin"] + 2*sim.humanRadius, roomCoordinates["ymax"] - 2*sim.humanRadius), sim.humanRadius), radius=sim.humanRadius, color=color.red)
-                person.waitCount = 0
+
 
 
 
                 if not sim.checkForCollision(person.pos.x, person.pos.y, rectangles, people):
                     people.append(person)
+                    person.waitCount = 0
+                    person.isUsingDoor = False
                     sim.plot_path(person, person.pos, rectangles, goalLocation, graph)
 
-                    # create random value between 0.3 and 1
+                    # create random value between min and 1
                     person.baseVelocity = sim.baseVelocity * random.uniform(sim.minInitialSpeedCoefficient, 1)
                     break
                 else:
@@ -481,7 +582,7 @@ class sim:
 
 
         # create timestep and loop over 10 seconds
-        dt = 0.01
+
         totalTime = 10
         counter = 0
 
@@ -489,31 +590,138 @@ class sim:
         # while people are not empty
         while len(people) > 0:
             # sets how many times this will run person per second
-            rate(1/dt)
+            rate(1/sim.dt)
             counter += 1
 
 
             # loop over all people
             for person in people:
                 moveX, moveY = sim.make_move(person, people)
-                person.pos.x += moveX*dt
-                person.pos.y += moveY*dt
+                person.pos.x += moveX*sim.dt
+                person.pos.y += moveY*sim.dt
 
 
         # calculate time taken
-        timeTaken = counter * dt
+        timeTaken = counter * sim.dt
 
 
         print("Time taken: " + str(timeTaken) + " seconds")
-        return timeTaken
+
+        return round(timeTaken, 2)
+
+    def reset_room(self):
+        sim.reset_variables(sim)
+        for item in sim.toDelete:
+            try:
+                item.visible = False
+            except:
+                print("")
+
+
+        sim.toDelete = []
+
+
+
+
+
+
+
+def build_csv(k, filepath, ascending: bool = True):
+
+
+
+
+
+
+    # check if figures/n_vs_time.csv exists, if not create it
+    if not os.path.exists(filepath):
+        with open(filepath, "w") as file:
+            # write column for n, room name, and time taken
+            file.write("n,room,time\n")
+
+    # load pandas from csv
+    df = pd.read_csv(filepath)
+
+    # create range of n integers
+    nValues = range(1, 180, 2)
+
+    # if ascending is false, reverse nValues
+    if not ascending:
+        nValues = reversed(nValues)
+
+
+    # loop over rooms
+    for room in sim.room.rooms:
+        # loop over n values
+        for n in nValues:
+
+
+            # if this n value is above the max capacity of the room, skip
+            if n > sim.room.roomCapacity[room]:
+                continue
+
+            # run k times
+            for i in range(k):
+
+                # get records with same n and room
+                similarRecords = df.loc[(df["n"] == n) & (df["room"] == sim.room.roomNumberToName[room])]
+
+                # if similar records is already k length, continue
+                if similarRecords.shape[0] >= k:
+                    continue
+
+
+                # -----------------run simulation-----------------#
+
+                # set sim.roomName to room
+                sim.roomName = room
+
+                # set sim.numHumans to n
+                sim.numHumans = int(n)
+
+                # run sim
+                graph, goalLocation, rectangles, roomCoordinates = sim.build(sim)
+                timeTaken = sim.run(graph, goalLocation, rectangles, roomCoordinates)
+
+                # reset room
+                sim.reset_room(sim)
+                sim.reset_variables(sim)
+
+                # if number of similar records in greater than 5 and time taken is more than 50% off current mean, continue
+                if similarRecords.shape[0] > 5 and abs(timeTaken - similarRecords["time"].mean()) > 0.5*similarRecords["time"].mean():
+                    print("Skipped run as outlier")
+                    continue
+
+                # add row to dataframe
+                df = df.append({"n": n, "room": sim.room.roomNumberToName[room], "time": timeTaken}, ignore_index=True)
+                df.to_csv(filepath, index=False)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
 
 if __name__ == '__main__':
+
+
     # run build
-    graph, goalLocation, rectangles, roomCoordinates = sim.build(sim)
-    sim.run(graph, goalLocation, rectangles, roomCoordinates)
+
+    build_csv(4, "figures/n_vs_time.csv")
+
+    # graph, goalLocation, rectangles, roomCoordinates = sim.build(sim)
+    # sim.run(graph, goalLocation, rectangles, roomCoordinates)
 
 
 
